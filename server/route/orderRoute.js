c497c83ef64ea043f2bf180bc512704fa95df3de
Meowmerry <meow3525@gmail.com>
2022-07-17 20:09:36 -0500

    done orderController

--- server/route/orderRoute.js
+++ server/route/orderRoute.js
@@ -7,10 +7,10 @@ router.post('/create-order',tokenVerifier, order.createOrder, (req, res)=>{
   return res.status(200).json('Success')
 })
 router.get('/orders/:userId',tokenVerifier, order.getBuyerOrders, (req, res)=>{
-  return res.status(200).json({})
+  return res.status(200).json(res.locals.orders)
 })
 
-// router.get('/orders:id',tokenVerifier, order.getBuyerOrders, (res, req)=>{
-//   return res.status(200).json({})
-// })
+router.get('/orderSales/:userId',tokenVerifier, order.getSellerOrders, (req, res)=>{
+  return res.status(200).json(res.locals.orders)
+})
 module.exports = router;
