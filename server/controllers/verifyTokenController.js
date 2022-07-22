81650e690cc9a715137ec7541b7bb9a0279d4723
Meowmerry <meow3525@gmail.com>
2022-07-22 12:11:11 -0500

    update information Seller and return back

--- server/controllers/verifyTokenController.js
+++ server/controllers/verifyTokenController.js
@@ -25,7 +25,6 @@ const tokenVerifier = (req, res, next) => {
 const tokenVerifier2 = (req, res, next) => {
 
   const token = req.cookies.token;
-  console.log('token-->',token)
   if (!token) {
     return res.sendStatus(403);
   }
