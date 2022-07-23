262b28ca1bd78ef074f8863771e5a195761710d4
Your Name <tonfroy90@gmail.com>
2022-07-23 11:17:12 -0700

    Pushing for work on Brandon

--- client/components/ProfileEdit.js
+++ client/components/ProfileEdit.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ConstructionOutlined } from '@mui/icons-material';
 import { makeStyles } from "@mui/styles";
 import Cooking from "../assets/kitchen1.jpg";
-import { Stack, Button, Box, CardContent, Paper, TextField, Typography } from "@mui/material";
+import { Stack, Button, Link, Box, CardContent, Paper, TextField, Typography } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   body: {
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
   heavyFont: {
     color: "white",
     fontWeight: "900",
-    fontSize: "40px",
+    fontSize: "100px",
     fontFamily: "Nunito",
   },
 }));
@@ -44,7 +44,11 @@ export default function ProfileEdit() {
         e.preventDefault()
         console.log("handleProfile post called")
 
-        axios.post("/db/updateProfile")
+        axios.post("/db/updateProfile",
+        {
+        chefName: chefName,
+        bio: bio,
+        })
         .then((res) => {
             console.log("response from axios:", res)
             if (res.status == 200) {
@@ -62,19 +66,19 @@ export default function ProfileEdit() {
                 flexWrap: 'wrap',
                 '& > :not(style)': {
                   m: 1,
-                  width: 700,
-                  height: 700,
+                  width: 500,
+                  height: 350,
                 },
               }}
             >
-                <h1> Tell us more, Chef </h1>
+                <h1 className={classes.heavyFont}> Tell us more, Chef </h1>
             <Paper elevation={3} >
                 <form onSubmit={handleProfile}>
             <Stack spacing={2}>
             <div>
             <h3> Whats your name? </h3>
             <TextField
-            
+          onChange={(e) => setAddChefName(e.target.value)}
           required
           id="outlined-required"
           label="Chef Name"
@@ -83,11 +87,32 @@ export default function ProfileEdit() {
         />
         <h3> Tell us more! </h3>
         <TextField
+        onChange={(e) => setAddBio(e.target.value)}
           required
           id="outlined-required"
           label="Bio"
           defaultValue=""
         />
+        <input
+                    accept="image/*"
+                    className={classes.profileInput}
+                    style={{ display: "none" }}
+                    id="raised-button-file"
+                    multiple
+                    type="file"
+                    onChange={e => setSelectedImage(e.target.files[0])}
+                />
+                <label htmlFor="raised-button-file">
+                    <Button variant="raised" component="span" className={classes.profileButton}>
+            Upload a Profile Picture
+                    </Button>
+                </label>
+                {imageUrl && selectedImage && (
+                    <Box mt={2} textAlign="center">
+                        <div>Image Preview:</div>
+                        <img src={imageUrl} alt={selectedImage.name} height="100px" />
+                    </Box>
+                )}
             </div>
             </Stack>
             </form>
